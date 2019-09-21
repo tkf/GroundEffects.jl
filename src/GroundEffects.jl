e09969c80d07a43f7f0839d3b7906fd7f18a288b
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-21 12:55:52 -0700

    Refactoring: make lowering customizable

--- src/GroundEffects.jl
+++ src/GroundEffects.jl
@@ -2,57 +2,101 @@ module GroundEffects
 
 using .Meta: isexpr
 
+@nospecialize
+
 """
     lower(ex::Expr) :: Expr
 
 Desugar subexpressions of `ex` as long as they are representable by Julia's
 surface syntax.  The lowered output can be used as an output of macro.
 """
-lower(ex::Expr) = _lower(ex)
+lower(ex::Expr) = lower(defaulthandlers(), ex)
+lower(handlers::AbstractVector, ex::Expr) = Dispatcher(handlers)(ex)
 
-_lower(x) = x
+abstract type AbstractHandler end
 
-function _lower(ex::Expr)
-    if ex.head === :macrocall
-        return ex
-    elseif ex.head === :vcat
-        if all(isexpr.(ex.args, :row))
-            rows = Tuple(length(a.args) for a in ex.args)
-            return :($(Base.hvcat)($rows, $((
-                _lower(r) for a in ex.args for r in a.args
-            )...)))
-        else
-            return :($(Base.vcat)($(map(_lower, ex.args)...)))
-        end
-    elseif ex.head === :hcat
-        return :($(Base.hcat)($(map(_lower, ex.args)...)))
-    elseif ex.head === :.=
-        @assert length(ex.args) == 2
-        a1, a2 = map(_lower, ex.args)
-        return :($(Base.materialize!)($a1, $(Base.broadcasted)(identity, $a2)))
-    elseif (m = match(r"^(\.)?(([^.]+)=)$", string(ex.head))) !== nothing
-        # e.g., `ex.head == :.+=`
-        op = Symbol(m.captures[3])  # e.g., `:+`
-        @assert length(ex.args) == 2
-        a1, a2 = map(_lower, ex.args)
-        if m.captures[1] === nothing
-            # e.g., `ex.head == :+=`
-            if !(a1 isa Symbol)
-                error("Assignment destination must be a symbol. Got:\n", a1)
-            end
-            return :($a1 = $op($a1, $a2))
-        elseif a1 isa Symbol
-            return :($(Base.materialize!)($a1, $(Base.broadcasted)($op, $a1, $a2)))
-        else
-            @gensym lhs
-            return Expr(
-                :block,
-                :($lhs = $a1),
-                :($(Base.materialize!)($lhs, $(Base.broadcasted)($op, $lhs, $a2))),
-            )
+defaulthandlers() = AbstractHandler[
+    Handler{:macrocall}(),
+    Handler{:vcat}(),
+    Handler{:hcat}(),
+    Handler{:.=}(),
+    DotUpdateHandler(),
+    RecursionHandler(),
+]
+
+struct Dispatcher
+    handlers::Vector{Any}
+end
+
+function (lower::Dispatcher)(ex)
+    for h in lower.handlers
+        if accept(h, ex)
+            return handle(h, lower, ex)
         end
     end
-    return Expr(ex.head, _lower.(ex.args)...)
+    return ex
 end
+
+struct Handler{head} <: AbstractHandler end
+
+accept(::Any, ::Any) = false
+accept(::Handler{head}, ex::Expr) where head = ex.head == head
+
+handle(::Handler{:macrocall}, _, ex) = ex
+
+function handle(::Handler{:vcat}, lower, ex)
+    if all(isexpr.(ex.args, :row))
+        rows = Tuple(length(a.args) for a in ex.args)
+        return :($(Base.hvcat)($rows, $((
+            lower(r) for a in ex.args for r in a.args
+        )...)))
+    else
+        return :($(Base.vcat)($(map(lower, ex.args)...)))
+    end
+end
+
+handle(::Handler{:hcat}, lower, ex) =
+    :($(Base.hcat)($(map(lower, ex.args)...)))
+
+function handle(::Handler{:.=}, lower, ex)
+    @assert length(ex.args) == 2
+    a1, a2 = map(lower, ex.args)
+    return :($(Base.materialize!)($a1, $(Base.broadcasted)(identity, $a2)))
+end
+
+struct DotUpdateHandler <: AbstractHandler end
+
+_dotupdatematch(ex) = match(r"^(\.)?(([^.]+)=)$", string(ex.head))
+
+accept(::DotUpdateHandler, ex::Expr) = _dotupdatematch(ex) !== nothing
+
+function handle(::DotUpdateHandler, lower, ex)
+    m = _dotupdatematch(ex)
+    # e.g., `ex.head == :.+=`
+    op = Symbol(m.captures[3])  # e.g., `:+`
+    @assert length(ex.args) == 2
+    a1, a2 = map(lower, ex.args)
+    if m.captures[1] === nothing
+        # e.g., `ex.head == :+=`
+        if !(a1 isa Symbol)
+            error("Assignment destination must be a symbol. Got:\n", a1)
+        end
+        return :($a1 = $op($a1, $a2))
+    elseif a1 isa Symbol
+        return :($(Base.materialize!)($a1, $(Base.broadcasted)($op, $a1, $a2)))
+    else
+        @gensym lhs
+        return Expr(
+            :block,
+            :($lhs = $a1),
+            :($(Base.materialize!)($lhs, $(Base.broadcasted)($op, $lhs, $a2))),
+        )
+    end
+end
+
+struct RecursionHandler <: AbstractHandler end
+
+accept(::RecursionHandler, ::Expr) = true
+handle(::RecursionHandler, lower, ex) = Expr(ex.head, map(lower, ex.args)...)
 
 end # module
